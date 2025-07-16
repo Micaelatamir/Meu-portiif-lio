4d967bf8ca3df64c406e82ba1d41ac379cf1841b
Micaela Tamires <micaela.tamir@gmail.com>
2025-07-16 19:49:59 -0300

    update index

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-const typedText = document.querySelector('.typed-text');
-const text = typedText.textContent;
-typedText.textContent = '';
-let index = 0;
+const typedText = document.getElementById("typed-text");
+const words = ["Desenvolvedora BackEnd"];
+let wordIndex = 0;
+let charIndex = 0;
 
-function typeText() {
-  if (index < text.length) {
-    typedText.textContent += text.charAt(index);
-    index++;
-    setTimeout(typeText, 100);
+function type() {
+  if (charIndex < words[wordIndex].length) {
+    typedText.textContent += words[wordIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, 100); // Velocidade da digitação
   }
 }
 
-typeText();
+document.addEventListener("DOMContentLoaded", () => {
+  typedText.textContent = "";
+  setTimeout(type, 300); // Pequena pausa antes de começar
+});
