55c7395931930856fe4c8e13426ba554fc205579
Micaela Tamires <micaela.tamir@gmail.com>
2025-07-17 09:43:55 -0300

    update

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-const typedText = document.getElementById("typed-text");
-const words = ["Desenvolvedora BackEnd"];
-let wordIndex = 0;
-let charIndex = 0;
+document.addEventListener("DOMContentLoaded", function () {
+  const text = ". Desenvolvedora Back-End ";
+  const element = document.getElementById("typed-text");
+  let index = 0;
 
-function type() {
-  if (charIndex < words[wordIndex].length) {
-    typedText.textContent += words[wordIndex].charAt(charIndex);
-    charIndex++;
-    setTimeout(type, 100); // Velocidade da digitação
+  function typeWriter() {
+    if (index < text.length) {
+      element.innerHTML += text.charAt(index);
+      index++;
+      setTimeout(typeWriter, 80);
+    }
   }
-}
 
-document.addEventListener("DOMContentLoaded", () => {
-  typedText.textContent = "";
-  setTimeout(type, 300); // Pequena pausa antes de começar
+  typeWriter();
 });
